3399a53068a4473f25a139e1b93d313319460b00
Gihun <563710@naver.com>
2023-08-11 15:15:05 +0900

    chore: init project

---
+++ src/main/java/com/wanted/onboarding/OnboardingApplication.java
@@ -0,0 +1,14 @@
+package com.wanted.onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingApplication.class, args);
+		System.out.printf("ㅎㅇㅎㅇ");
+	}
+
+}
