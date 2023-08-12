61d4f27b74037641b55523e8e44ab7a036527df8
Ropung <563710@naver.com>
2023-08-12 21:13:57 +0900

    fix(memeber): Entity에서 기본값의 책임을 Service 비즈로직에서 매퍼를 통해 책임을 전가함

--- src/main/java/com/wanted/onboarding/member/domain/Member.java
+++ src/main/java/com/wanted/onboarding/member/domain/Member.java
@@ -1,9 +1,13 @@
 package com.wanted.onboarding.member.domain;
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
 import com.wanted.onboarding.support.BaseEntity;
 import com.wanted.onboarding.member.domain.type.MemberStatus;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -12,13 +16,15 @@ import javax.persistence.Enumerated;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Member extends BaseEntity {
 
     private String email;
     private String password;
     private String name;
-
     @Enumerated(EnumType.STRING)
-    private MemberStatus status= MemberStatus.ACTIVE;
+    private MemberStatus status;
 
 }
