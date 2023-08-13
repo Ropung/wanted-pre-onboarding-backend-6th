b398e6c2363485fbc451fa09df1c03a9c40b6e67
Ropung <563710@naver.com>
2023-08-13 21:23:47 +0900

    feat(DDL): Add tb_board, Modify tb_member

---
+++ src/main/resources/db/migration/V0.0.1__create_tb_board.sql
@@ -0,0 +1,9 @@
+  create table wanted.board (
+    id              bigint              not null        auto_increment,
+    member_id       bigint              not null,
+    content         varchar(255),
+    title           varchar(255),
+    created_at      datetime(6),
+    updated_at      datetime(6),
+    primary key (id)
+  )
