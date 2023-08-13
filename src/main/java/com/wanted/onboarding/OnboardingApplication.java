5284b4897dd66cc5a22ba1c42987dd2106e0c648
Ropung <563710@naver.com>
2023-08-13 19:58:40 +0900

    feat(app): Bean Search(JwtProperties.class)

--- src/main/java/com/wanted/onboarding/OnboardingApplication.java
+++ src/main/java/com/wanted/onboarding/OnboardingApplication.java
@@ -1,9 +1,16 @@
 package com.wanted.onboarding;
 
+import com.wanted.onboarding.properties.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class OnboardingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OnboardingApplication.class, args);
